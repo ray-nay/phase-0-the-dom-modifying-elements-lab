76aa97424685107ce2aac3031e3a1aa23f614d41
Renee Mumbi <1723renee@gmail.com>
2022-12-18 16:15:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove();
+//you can either use (main.remove) all main elements or remove main using the id as below
+//document.getElementById("main").remove()
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Renee is the champion"
+
